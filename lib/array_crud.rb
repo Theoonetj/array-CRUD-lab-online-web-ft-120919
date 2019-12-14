aa45ff6ff5023387863cdba79044a1125e6f361f
confident-token-1019 <theoonetj@gmail.com>
2019-12-14 18:04:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,17 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  array = ["john", 1, "Peter", 2]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array<< "Abram"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift("Gabe")
 end
 
 def remove_element_from_end_of_array(array)
