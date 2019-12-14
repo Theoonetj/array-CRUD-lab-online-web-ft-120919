a07a6c57c970cc557ca5184620f23c7eac45cd70
confident-token-1019 <theoonetj@gmail.com>
2019-12-14 18:27:22 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,40 @@
 def create_an_empty_array
-  
+  array = []
 end
 
 def create_an_array
-  
+  array = ["john", 1, "Peter", 2]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(array, element = "Abram")
+  array<< element
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(array, element "Gabe")
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
+  
+  array.pop
   
 end
 
 def remove_element_from_start_of_array(array)
   
+  array.shift
+  
 end
 
 def retrieve_element_from_index(array, index_number)
+  array[2]
   
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
